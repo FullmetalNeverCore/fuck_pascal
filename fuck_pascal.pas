2b3aea11916ef2946e5259734bc8dcf9cf9d2054
FullmetalNeverCore <60969891+FullmetalNeverCore@users.noreply.github.com>
2020-11-08 18:29:10 +0200

    fuck_pascal

---
+++ fuck_pascal.pas
@@ -0,0 +1,15 @@
+﻿program fuck_pascal;
+var
+  a: string;
+  y : real;
+ 
+begin 
+  read(a);
+  y := 0; 
+  if (a = 'Fuck Pascal') then
+    write('Yea,fuck pascal!')
+  else 
+    while y < 10 do
+        write('Fuck Pascal!');
+        y := y + 1;
+end.
